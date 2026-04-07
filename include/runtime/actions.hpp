c8e8e480be02edb85d574de0c15dcdff33e526d4
PeyloW <peylow@gmail.com>
2026-04-07 16:22:01 -0700

    Adding support for counter and flags entitiues. Reuired a bit of refactoring, as flags entity needs the source entity for the action callback.

--- include/runtime/actions.hpp
+++ include/runtime/actions.hpp
@@ -13,10 +13,10 @@
 namespace toybox {
     
     class tilemap_level_c;
-    using action_f = void(*)(tilemap_level_c& level, entity_s& entity, bool event);
+    using action_f = void(*)(tilemap_level_c& level, entity_s& entity, uint8_t source);
     
     namespace actions {
-        static void idle(tilemap_level_c& level, entity_s& entity, bool event) {}
+        static void idle(tilemap_level_c& level, entity_s& entity, uint8_t source) {}
     }
     
 }
--- sample/src/tilemap_scene.cpp
+++ sample/src/tilemap_scene.cpp
@@ -46,7 +46,7 @@ static bool move_entity_if_possible(tilemap_level_c& level, entity_s& entity, fp
     return false;
 }
 
-static void player_control(tilemap_level_c& level, entity_s& entity, bool event) {
+static void player_control(tilemap_level_c& level, entity_s& entity, uint8_t source) {
     auto dir = controller_c::shared().directions();
     fpoint_s delta(0,0);
     if ((dir & controller_c::up) == true) {
